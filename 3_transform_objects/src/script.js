59649c5a5a208cdfbff17c6730a05339d33cf61e
nirajan1111 <nirajansah1111@gmail.com>
2024-01-22 22:10:54 +0545

    group of cube

--- 3_transform_objects/src/script.js
+++ 3_transform_objects/src/script.js
@@ -6,21 +6,63 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// mesh.position.x = 0.7
+// mesh.position.y = -0.7
+// mesh.position.z = 1
 
+// mesh.rotation.reorder('YX/Z')
+// mesh.rotation.x=Math.PI *0.25
+// mesh.rotation.y = Math.PI/2
+
+// // mesh.position.set(0.7, -0.7, 1)
+// // console.log(mesh.position.length())
+// // mesh.position.normalize() // it make the position 1 
+// // console.log(mesh.position.length())
+
+
+// mesh.scale.x=2
+// scene.add(mesh)
+
+
+
+
+const group = new THREE.Group()
+scene.add(group)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+
+cube2.position.x = -1.5
+cube3.position.x = 1.5
+group.add(cube1)
+group.add(cube2)
+group.add(cube3)
+
+group.position.y = 2
+group.rotation.y = 1
+const AxisHelper = new THREE.AxesHelper()
+scene.add(AxisHelper)
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: 1000,
+    height: 800
 }
 
 /**
@@ -28,7 +70,12 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.y = 1
+camera.position.x = 1
+// camera.lookAt(mesh.position)  
 scene.add(camera)
+
+// console.log(mesh.position.distanceTo(camera.position))
 
 /**
  * Renderer
