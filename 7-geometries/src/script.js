b6729d9d97fa6e823df1f84e866bf92600c3a95c
nirajan1111 <nirajansah1111@gmail.com>
2024-02-01 11:45:32 +0545

    geometry

--- 7-geometries/src/script.js
+++ 7-geometries/src/script.js
@@ -12,8 +12,28 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const geometry = new THREE.BoxGeometry(1, 1, 1,4,4,4)
+// const positionsArray = new Float32Array(
+//     [
+//         0,0,0,
+//         0,1,0,
+//         1,0,0
+//     ]
+// )
+// const positionsAttribute = new THREE.BufferAttribute(positionsArray,3)
+const count = 500
+const positionsArray = new Float32Array(count*3*3)
+for(let i=0;i<count*3*3;i++){
+    positionsArray[i] = (Math.random()-0.5)*4
+}
+const positionsAttribute = new THREE.BufferAttribute(positionsArray,3)
+const geometry = new THREE.BufferGeometry()
+geometry.setAttribute('position',positionsAttribute)
+
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xff0000 ,
+    wireframe:true
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
